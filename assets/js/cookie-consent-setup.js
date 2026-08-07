a2bc53f609120702273c6d2beabaf295e9b9da40
jaechang <jaechang@postech.ac.kr>
2026-08-07 13:07:14 +0900

    update postdoc

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://jaechang.dev/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="http://localhost:4000/#contact">contact us</a>.'
               }
             ]
           }
